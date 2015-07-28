837ed8eb6f5d68b1fcc048d31c7d5246e496136b
nachkari <nickachkarian@gmail.com>
2015-07-28 12:41:05 -0400

    Level 2 AI basic
    lmao

--- pp9k/pp9k/computer.cc
+++ pp9k/pp9k/computer.cc
@@ -6,9 +6,9 @@ bool Computer::movePiece() {
     switch (level) {
         case 1:
             {
-if(turn == 0){
+if(turn == 1){
     // fill an array from 1 to length
-    int length = p1pieces.size();
+    int length = game->p1pieces.size();
 	int arr[length];
 	for(int i = 0; i < length; i++)
 		arr[i] = i;
@@ -25,7 +25,7 @@ if(turn == 0){
 		vector<int> valid;
 		for(int j = 1; j < 9; j++){
 			for(int k = 1; k < 9; k++){
-			if(p1pieces.at(i).isValidMove(calcPosition(j,k)))
+			if(game->p1pieces.at(i).isValidMove(calcPosition(j,k)))
 				valid.push_back(calcPosition(j,k));
 			}
 		}
@@ -33,7 +33,86 @@ if(turn == 0){
 				
 			if(valid.size() != 0){
 				int k = rand() % len;
-				p1pieces.at(i).move(valid.at(k));
+				game->p1pieces.at(i).move(valid.at(k));
+				return 1;
+			}
+		i++;
+
+	}//while
+
+	}
+else{
+	int length = game->p1pieces.size();
+	int arr[length];
+	for(int i = 0; i < length; i++)
+		arr[i] = i;
+	//random swap
+	for(int i = 0; i < length; i++){
+		int ran = rand() % length - 1;
+		int temp = arr[i];
+		arr[i] = arr[ran];
+		arr[ran] = temp;
+	}
+
+	while(i < length){
+		vector<int> valid;
+
+		for(int j = 1; j < 9; j++){
+			for(int k = 1; k < 9; k++){
+			if(game->p1pieces.at(i).isValidMove(calcPosition(j,k)))
+				valid.push_back(calcPosition(j,k));
+			}
+		}
+			
+		if(valid.size() != 0){
+			int k = rand() % len;
+			game->p2pieces.at(i).move(valid.at(k));
+			return 1;
+		}
+		i++;
+
+	}//while
+
+}
+return 0;
+}
+            break;
+            
+        case 2:
+            if(turn == 1){
+    // fill an array from 1 to length
+    int length = game->p1pieces.size();
+	int arr[length];
+	for(int i = 0; i < length; i++)
+		arr[i] = i;
+
+	//random swap so the order of numbers is random
+	for(int i = 0; i < length; i++){
+		int ran = rand() % length - 1;
+		int temp = arr[i];
+		arr[i] = arr[ran];
+		arr[ran] = temp;
+	}
+	//checks all positions for which are valid positions for the given piece, stores all valid in a vector
+	while(i < length;){
+		vector<int> valid;
+		length1 = game->p2pieces.size()
+		//finds all valid attacks, stores in first parts of stack
+		for(int j = 1; j < length1; j++){
+			if(game->p1pieces.at(i).isValidMove(game->p2pieces.at(j).getPos()))
+				valid.push_back(calcPosition(j,k));
+		}
+		//finds rest of valid moves, stores after valid attacks
+		for(int j = 1; j < 9; j++){
+			for(int k = 1; k < 9; k++){
+			if(game->p1pieces.at(i).isValidMove(calcPosition(j,k)))
+				valid.push_back(calcPosition(j,k));
+			}
+		}
+	//checks all positions for which are valid positions for the given piece, stores all valid in a vector
+				//returns the first move
+			if(valid.size() != 0){
+				game->p1pieces.at(i).move(valid.at(1));
 				return 1;
 			}
 		i++;
@@ -53,22 +132,28 @@ else{
 		arr[i] = arr[ran];
 		arr[ran] = temp;
 	}
-
-	while(i < length){
+	//checks all positions for which are valid positions for the given piece, stores all valid in a vector
+	while(i < length;){
 		vector<int> valid;
-
+		length1 = game->p1pieces.size()
+		//finds all valid attacks, stores in first parts of stack
+		for(int j = 1; j < length1; j++){
+			if(game->p2pieces.at(i).isValidMove(game->p1pieces.at(j).getPos()))
+				valid.push_back(calcPosition(j,k));
+		}
+		//finds rest of valid moves, stores after valid attacks
 		for(int j = 1; j < 9; j++){
 			for(int k = 1; k < 9; k++){
-			if(p1pieces.at(i).isValidMove(calcPosition(j,k)))
+			if(game->p2pieces.at(i).isValidMove(calcPosition(j,k)))
 				valid.push_back(calcPosition(j,k));
 			}
 		}
-			
-		if(valid.size() != 0){
-			int k = rand() % len;
-			p2pieces.at(i).move(valid.at(k));
-			return 1;
-		}
+	//checks all positions for which are valid positions for the given piece, stores all valid in a vector
+				//returns the first move2
+			if(valid.size() != 0){
+				game->p1pieces.at(i).move(valid.at(1));
+				return 1;
+			}
 		i++;
 
 	}//while
@@ -76,10 +161,6 @@ else{
 }
 return 0;
 }
-            break;
-            
-        case 2:
-            
             break;
             
         case 3:
